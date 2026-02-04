dfb7aa5a2de7a8d813c7a51f57c622831c23694d
Louis Vincelli <louisvincelli@gmail.com>
2026-02-04 18:07:32 -0500

    Initial Commit

---
+++ MapKitApp/MapKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppApp.swift
+//  MapKitApp
+//
+//  Created by Louis Vincelli on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct MapKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapKitAppTests/MapKitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitAppTests.swift
+//  MapKitAppTests
+//
+//  Created by Louis Vincelli on 2/4/26.
+//
+
+import Testing
+@testable import MapKitApp
+
+struct MapKitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
